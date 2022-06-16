7b92578f337a10853d872d5773ad469fe8567100
CblnBomb <107231201+CblnBomb@users.noreply.github.com>
2022-06-16 11:39:53 +0900

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,11 +1,20 @@
-for( let i = 1; i <= 100; i + 1 ) {
+function fizzbuzz(number) {
+ for( let i = 0; i <= number; i ++ ) {
   if( i % 15 === 0 ) {
     console.log( "FizzBuzz" );
-   } else if ( i % 5 === 0 ) {
+   }
+   else if ( i % 5 === 0 ){
     console.log( "Buzz" );
-   } else if ( i % 3 === 0 ) {
+   }
+   else if ( i % 3 === 0 ){
     console.log( "Fizz" );
-   } else {
+   }
+   else {
     console.log( i );
    }
+  }
 }
+
+var test = 20;
+
+fizzbuzz(test);
